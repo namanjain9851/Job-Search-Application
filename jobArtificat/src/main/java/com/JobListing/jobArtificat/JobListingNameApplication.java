651e85b16b6da1d7bb6c250780b7cf54cd6dbeba
Naman Jain <155815479+namanjain9851@users.noreply.github.com>
2024-02-09 01:19:10 +0530

    Add files via upload

---
+++ jobArtificat/src/main/java/com/JobListing/jobArtificat/JobListingNameApplication.java
@@ -0,0 +1,13 @@
+package com.JobListing.jobArtificat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobListingNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobListingNameApplication.class, args);
+	}
+
+}
---
+++ jobArtificat/src/test/java/com/JobListing/jobArtificat/JobListingNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JobListing.jobArtificat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
